2cd4945049477327466ccbdc38ca46559fa1a5e1
Tanner Birkenfeld <tanner.birkenfeld@nazarethisd.org>
2022-11-28 13:51:13 -0600

    16617 - RoadRunner DC tuned in safety catch servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de39c718fa4a7494714c176c9d6d3260740870ce0108a03e9bc5e50351f6f936
-size 5398
+oid sha256:286f9e8b844e9118ae4cba902b975eb243fc786c84c261429c5c1de8e34fb6e1
+size 5507
