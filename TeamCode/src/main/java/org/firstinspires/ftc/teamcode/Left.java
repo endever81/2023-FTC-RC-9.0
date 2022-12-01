285f1a71c0413290204995885899a6a3cd4b1cbc
Tanner Birkenfeld <tanner.birkenfeld@nazarethisd.org>
2022-12-01 09:18:33 -0600

    16617 - RoadRunner AUTO began adding code to pick up second cone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Left.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f537fcb0fe039d9ef47fb3cdd3044ff8e9531a65e2d387979d93c566d1648648
-size 32042
+oid sha256:cf700ced130b797907e7cb2127e224bf6793ca4ae38e8ce58e930104de02d61c
+size 32151
